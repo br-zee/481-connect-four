820bd4a86182fb8ce6bd4cbbf08741d5153f5283
HunterTran10 <hunter.q.tran@gmail.com>
2025-12-05 16:00:07 -0800

    added choose move method

--- Player.py
+++ Player.py
@@ -12,3 +12,22 @@ class Computer(Player):
 
     def __init__(self, id, name="Computer"):
         super().__init__(id, name)
+        
+    def choose_move(self, game, opponent_id):
+        from minimax import minimax
+        
+        valid_columns = game.getValidColumns()
+        best_score = float('-inf')
+        best_col = valid_columns[0]
+        
+        for col in valid_columns:
+            sim_game, row = game.simulatedDrop(col, self.id)
+            
+            if sim_game:
+                score = minimax(sim_game, 4, False, self.id, opponent_id)
+                
+                if score > best_score:
+                    best_score = score
+                    best_col = col
+        
+        return best_col
